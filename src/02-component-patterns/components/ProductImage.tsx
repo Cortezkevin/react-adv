e8473ba427e3f3a9c5eaeb728c312a4665463cd1
Cortezkevin <A19105415@idat.edu.pe>
2023-01-21 09:01:11 -0500

    Component Compound

---
+++ src/02-component-patterns/components/ProductImage.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+import { ProductContext } from './ProductCard';
+import noImage from '../assets/no-image.jpg';
+import styles from '../styles/styles.module.css';
+
+export const ProductImage = ({ img = '' }) => {
+
+	const { product } = useContext( ProductContext );
+	let imgToShow: string;
+
+	if( img ){
+		imgToShow = img;
+	}else if( product.image ){
+		imgToShow = product.image;
+	}else{
+		imgToShow = noImage;
+	}
+
+	return (
+		<img className={ styles.productImg } src={ imgToShow } alt="Cofee Mug" />
+	)
+}
