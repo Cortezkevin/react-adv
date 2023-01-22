35b7ae7ad6fc17a11cead90fbab0199ee3464d10
Cortezkevin <A19105415@idat.edu.pe>
2023-01-22 12:07:38 -0500

    Control props alternative

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { Product, onChangeArgs } from "../interfaces/interfaces";
 
 interface useProductArgs {
@@ -10,12 +10,8 @@ interface useProductArgs {
 export const useProduct = ( { onChange, product, value = 0 }: useProductArgs ) => {
 
 	const [counter, setCounter] = useState( value );
-  const isControlled = useRef( !!onChange );
 
-	const increaseBy = ( value: number ) => {
-    if( isControlled.current ){
-      return onChange!({ count: value, product });
-    }
+	const increaseBy = ( value: number ) => {    
     const newValue = Math.max( counter + value, 0);
 
 		setCounter( newValue );
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -8,7 +8,7 @@ export const useShoppingCart = () => {
 	const onProductCountChange = ({ count, product }: onChangeArgs) => {
 		setShoppingCart(prev => {
 			
-			const productInCart: ProductInCart = prev[ product.id ] || { ...product, count: 0 };
+			/* const productInCart: ProductInCart = prev[ product.id ] || { ...product, count: 0 };
 
 			if( Math.max( productInCart.count + count, 0 ) > 0){
 				productInCart.count += count;
@@ -19,7 +19,17 @@ export const useShoppingCart = () => {
 			}
 
 			const { [product.id]: toDelete, ...rest } = prev;
-			return rest;
+			return rest; */
+
+			if( count === 0 ){
+				const { [product.id]: toDelete, ...rest } = prev;
+				return rest; 
+			}
+
+			return {
+				...prev,
+				[ product.id ]: { ...product, count }
+			}
 		});
 	}
   return {
